0b027fe7fb164a5ec17e6c68b07b4d7e7113cacc
Yakshit Poojary <yakshitpoojary2004@gmail.com>
2024-10-25 21:40:29 +0530

    Create insecure_deserialization.py

---
+++ insecure_deserialization.py
@@ -0,0 +1,17 @@
+import pickle
+
+# Example of data received from an untrusted source (user input, network, etc.)
+# Assume 'data' comes from an untrusted source
+data = b"cos\nsystem\n(S'echo Insecure Deserialization!'\ntR."
+
+# Deserialize the data
+try:
+    obj = pickle.loads(data)
+    print("Deserialized object:", obj)
+except Exception as e:
+    print("Deserialization failed:", e)
+
+# JSON deserialization (safe alternative)
+data = '{"key": "value"}'
+obj = json.loads(data)
+print("Deserialized object:", obj)
